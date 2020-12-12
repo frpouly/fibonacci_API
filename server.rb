c051c18d6f870a6928230984dd2013c7be488c99
frpouly <francois.pouly@etu.uca.fr>
2020-12-12 12:34:56 +0100

    First "working" version of the API, needs a lot of upgrades

--- server.rb
+++ server.rb
@@ -1,6 +1,21 @@
 require 'sinatra'
 
-get '/' do
+def fibonacci(num)
+  first_num, second_num = [0, 1]
+  (num - 1).times do
+    first_num, second_num = second_num, first_num + second_num
+  end
+  first_num
+end
+
+before do
   content_type 'application/json'
+end
+
+get '/' do
   "hello world".to_json
+end
+
+get '/api/v1/fibonacci/:number' do
+  fibonacci(Integer("#{params[:number]}")).to_json
 end
