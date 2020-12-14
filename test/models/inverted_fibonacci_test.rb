f04d2b67553721bdc1d40905cb0f73791f5fa133
frpouly <francois.pouly@etu.uca.fr>
2020-12-15 00:09:07 +0100

    Added the tests for the models

---
+++ test/models/inverted_fibonacci_test.rb
@@ -0,0 +1,15 @@
+require 'test/unit'
+
+class InvertedFibonacciTest < Test::Unit::TestCase
+  def setup
+    # Do nothing
+  end
+
+  def teardown
+    # Do nothing
+  end
+
+  def test
+    pend 'Not implemented'
+  end
+end
